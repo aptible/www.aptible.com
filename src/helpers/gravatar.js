6de5cad2822c4f44aaa408447e6452e8a9e99d21
Frank Macreery <frank@macreery.com>
2014-07-23 09:28:01 -0700

    Switch to secure Gravatar URLs (fixes #38)

--- src/helpers/gravatar.js
+++ src/helpers/gravatar.js
@@ -1,4 +1,4 @@
-var gravatar = require('node-gravatar'),
+var gravatar = require('gravatar'),
   _ = require('lodash');
 
 module.exports.register = function (Handlebars, options) {
@@ -8,7 +8,7 @@ module.exports.register = function (Handlebars, options) {
     size = size || 'sm';
 
     var sizes = { xs: 16, sm: 24, md: 40, lg: 64, xl: 100, xxl: 280 },
-      gravatar_url = gravatar.get(email, 'G', sizes[size]),
+      gravatar_url = gravatar.url(email, { r: 'G', s: sizes[size] }, true),
       html = _.template('<div class="gravatar <%= size %>"><img src="<%= gravatar_url %>" /></div>')({ size: size, gravatar_url: gravatar_url});
 
       return new Handlebars.SafeString(html);
