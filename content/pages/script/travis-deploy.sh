4941f53169814bf3339255396b66ee769f09f481
Frank Macreery <frank@macreery.com>
2015-04-30 18:04:27 -0400

    Make sure to only rebuild master branch builds

--- content/pages/script/travis-deploy.sh
+++ content/pages/script/travis-deploy.sh
@@ -6,7 +6,10 @@ set -e
 if [ "$TRAVIS_BRANCH" == "master" ] &&
    [ "$TRAVIS_PULL_REQUEST" == "false" ] ; then
    gem install -N travis
-   travis restart -t "$TRAVIS_TOKEN" \
-                  -r aptible/www.aptible.com \
-                  --skip-completion-check
+
+   lastbuild=$(travis branches -r aptible/www.aptible.com | \
+               egrep ^master: | awk '{ print $2 }' | tr -d '#')
+   travis restart "$lastbuild" -t "$TRAVIS_TOKEN" \
+                               -r aptible/www.aptible.com \
+                               --skip-completion-check
 fi
