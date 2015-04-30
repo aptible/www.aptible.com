7e5a477bea8dc3d2bc839a104d96be97fb3c205c
Frank Macreery <frank@macreery.com>
2015-04-30 12:33:05 -0400

    Move install steps into travis-deploy.sh for fast (no-op) PR builds

--- script/travis-deploy.sh
+++ script/travis-deploy.sh
@@ -6,5 +6,15 @@ set -e
 # TODO: Only build after merge to master, not on PRs
 if [ "$TRAVIS_BRANCH" == "master" ] &&
    [ "$TRAVIS_PULL_REQUEST" == "false" ] ; then
+
+  # Setup/install
+  bundle install --deployment --retry 3
+  npm install
+  npm install bower
+  bower install
+  grunt setup
+  pip install --user awscli
+
+  # Deploy to www.aptible-staging.com
   grunt release:staging
 fi
