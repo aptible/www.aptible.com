c3468b6b85c78f7a527d3f61f2bb8a2ff21f5299
Frank Macreery <frank@macreery.com>
2015-05-06 16:40:06 -0400

    Add --skip-completion-check and log (prefix of) token for debugging

--- content/pages/script/travis-deploy.sh
+++ content/pages/script/travis-deploy.sh
@@ -7,9 +7,13 @@ if [ "$TRAVIS_BRANCH" == "master" ] &&
    [ "$TRAVIS_PULL_REQUEST" == "false" ] ; then
    gem install -N travis
 
-   lastbuild=$(travis branches -r aptible/www.aptible.com | \
+   WWW=aptible/www.aptible.com
+   lastbuild=$(travis branches -r $WWW --skip-completion-check | \
                egrep ^master: | awk '{ print $2 }' | tr -d '#')
+
+   echo -n "Using TRAVIS_TOKEN "
+   echo $TRAVIS_TOKEN | cut -c 1-6
    travis restart "$lastbuild" -t "$TRAVIS_TOKEN" \
-                               -r aptible/www.aptible.com \
+                               -r $WWW \
                                --skip-completion-check
 fi
