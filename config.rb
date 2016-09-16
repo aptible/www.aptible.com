16d25b0e168b99c8b313beb9630e86f7d4f8372b
gib <gib.reimschussel@gmail.com>
2016-09-15 17:08:00 -0700

    rubocop: fix comments, match indent

--- config.rb
+++ config.rb
@@ -65,18 +65,18 @@ page '/*.txt', layout: false
 # Requires the site to be "ready" to read from the sitemap resources
 page '/blog/*', layout: 'blog_post.haml'
 # ready do
-  # binding.pry
-  # Posts
-  # sitemap.resources.select { |p| p.data['section'] == 'Blog' }.each do |post|
-  #   page "/#{post.path}", layout: 'blog_post.haml'
-  # end
-
-  # # Authors
-  # sitemap.resources.group_by { |p| p.data['author_id'] }
-  #                   .each do |author_id, posts|
-  #   page "/blog/authors/#{author_id}/index.html", layout: 'blog_author.haml',
-  #     :locals => { :author_id => author_id, :posts => posts }
-  # end
+#   binding.pry
+#   Posts
+#   sitemap.resources.select { |p| p.data['section'] == 'Blog' }.each do |post|
+#     page "/#{post.path}", layout: 'blog_post.haml'
+#   end
+#
+#   # Authors
+#   sitemap.resources.group_by { |p| p.data['author_id'] }
+#                     .each do |author_id, posts|
+#     page "/blog/authors/#{author_id}/index.html", layout: 'blog_author.haml',
+#       :locals => { :author_id => author_id, :posts => posts }
+#   end
 # end
 
 # Legal
