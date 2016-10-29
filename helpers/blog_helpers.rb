b8005f3ebb81c824279a43505b1e999bcd5c49cb
Chas Ballew <chas@aptible.com>
2016-10-29 13:51:48 -0600

    Fix ordering for previous/next blog posts

--- helpers/blog_helpers.rb
+++ helpers/blog_helpers.rb
@@ -1,28 +1,35 @@
 module BlogHelpers
-  def latest_blog_post
-    blog_posts_by_date.first
-  end
-
   def blog_post_author_href(post)
     "/blog/authors/#{post.data.author_id}"
   end
 
-  def blog_posts_by_date
+  def blog_posts_oldest_first
     sitemap.resources
            .select { |p| p.data['section'] == 'Blog' }
-           .sort_by { |p| p.data['posted'] }.reverse!
+           .sort_by { |p| p.data['posted'] }
+  end
+
+  def blog_posts_newest_first
+    blog_posts_oldest_first.reverse!
+  end
+
+  def latest_blog_post
+    blog_posts_newest_first.first
+  end
+
+  def index_of_post(post)
+    blog_posts_oldest_first.index { |p| p.path == post.path }
   end
 
   def prev_post(current_post)
-    ordered_posts = blog_posts_by_date
-    current = ordered_posts.index { |p| p.path == current_post.path }
-    current > 0 ? ordered_posts.at(current - 1) : nil
+    if index_of_post(current_post) > 0
+      return blog_posts_oldest_first.at(index_of_post(current_post) - 1)
+    end
+    nil
   end
 
   def next_post(current_post)
-    ordered_posts = blog_posts_by_date
-    current = ordered_posts.index { |p| p.path == current_post.path }
-    ordered_posts.at current + 1
+    blog_posts_oldest_first.at index_of_post(current_post) + 1
   end
 
   def post_date(string_date)
