49fe5cf9f0d2ace27385736225c1932a11cb6324
gib <gib.reimschussel@gmail.com>
2016-12-23 09:11:25 -0700

    Spec coverage for robots.txt and sitemap.xml

---
+++ spec/features/sitemap_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe 'sitemap.xml', type: :feature do
+  before do
+    visit '/sitemap.xml'
+  end
+
+  it 'has an XML doctype' do
+    page.html.should include('<?xml version="1.0" encoding="UTF-8"?>')
+  end
+
+  it 'excludes the ui-kit' do
+    page.should_not have_content(/ui-kit/)
+  end
+
+  it 'lists all pages in the site' do
+    sections_regex = /blog|company|customers|enclave|faq|gridiron|supprt/
+    page.should have_content sections_regex
+  end
+end
