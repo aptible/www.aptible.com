4307c45554633634edb8974d82364243ad2ffe1d
mjp <peterson514@gmail.com>
2018-05-25 09:57:35 -0400

    Include UTM params in analytics events

--- aptiblejs/src/analytics.js
+++ aptiblejs/src/analytics.js
@@ -1,13 +1,67 @@
+export const utmVars = ['utm_source', 'utm_medium', 'utm_campaign', 'utm_content'];
+
+export function allParams() {
+  let params = {};
+
+  const queryString = (window.location.search[0] === '?' ? window.location.search.substr(1) : window.location.search);
+  if (queryString.length < 1) {
+    return params;
+  }
+
+  const pairs = queryString.split('&');
+  for (let i = 0; i < pairs.length; i++) {
+    let pair = pairs[i].split('=');
+    params[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+  }
+
+  return params;
+}
+
+export function getParam(key) {
+  return allParams()[key];
+}
+
+export function allUtmVars() {
+  let utms = {};
+
+  const params = allParams();
+  for (let key in params) {
+    if (utmVars.indexOf(key) !== -1) {
+      utms[key] = params[key];
+    }
+  }
+
+  return utms;
+}
+
+export function currentURL() {
+  return `${window.location.origin}${window.location.pathname}`;
+}
+
 export function event(name) {
   if (window.analytics) {
-    window.analytics.track(name);
+    let payload = allUtmVars();
+    payload['url'] = currentURL();
+
+    window.analytics.track(name, payload);
   }
 }
 
 export function identify(email) {
   if (window.analytics) {
     window.analytics.identify(email, {
-      email: email
+      email: email,
+      url: currentURL()
     });
+  }
+}
+
+export function firePixel(service) {
+  switch (service) {
+    case 'twitter':
+      if (window.twttr) {
+        twttr.conversion.trackPid('nz8pr', { tw_sale_amount: 0, tw_order_quantity: 0 });
+      }
+      break;
   }
 }
