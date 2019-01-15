483af3c92a7f2a3696556494cde00652a2e9a70f
mjp <peterson514@gmail.com>
2019-01-15 12:15:22 -0500

    Blurred out action items

--- quizjs/src/components/presentation/ResultsActionItems.js
+++ quizjs/src/components/presentation/ResultsActionItems.js
@@ -3,6 +3,10 @@ import React from 'react';
 const ResultsActionItems = ({ score }) => (
   <div className="quiz__results__action-items">
     <h4>Action Items:</h4>
+
+    <div className="quiz__results__action-items--blurred">
+      <a href="#" className="btn btn--enclave">Click here to unlock</a>
+    </div>
   </div>
 );
 
