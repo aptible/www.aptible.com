21e5b102bd84a626ce88c0967b9b460a118c9201
mjp <peterson514@gmail.com>
2019-01-23 09:47:27 -0500

    htmlFor instead of for

--- quizjs/src/components/presentation/Likert.js
+++ quizjs/src/components/presentation/Likert.js
@@ -3,27 +3,27 @@ import React from 'react';
 const Likert = ({ questionId, recordAnswer }) => (
   <div className="quiz__likert-options">
     <div className="quiz__likert-option">
-      <label for={`question-${questionId}-0`}>
+      <label htmlFor={`question-${questionId}-0`}>
         <input type="radio" id={`question-${questionId}-0`} name={`question-${questionId}`} onChange={() => recordAnswer(questionId, 0)} />
       </label>
     </div>
     <div className="quiz__likert-option">
-      <label for={`question-${questionId}-1`}>
+      <label htmlFor={`question-${questionId}-1`}>
         <input type="radio" id={`question-${questionId}-1`} name={`question-${questionId}`} onChange={() => recordAnswer(questionId, 1)} />
       </label>
     </div>
     <div className="quiz__likert-option">
-      <label for={`question-${questionId}-2`}>
+      <label htmlFor={`question-${questionId}-2`}>
         <input type="radio" id={`question-${questionId}-2`} name={`question-${questionId}`} onChange={() => recordAnswer(questionId, 2)} />
       </label>
     </div>
     <div className="quiz__likert-option">
-      <label for={`question-${questionId}-3`}>
+      <label htmlFor={`question-${questionId}-3`}>
         <input type="radio" id={`question-${questionId}-3`} name={`question-${questionId}`} onChange={() => recordAnswer(questionId, 3)} />
       </label>
     </div>
     <div className="quiz__likert-option">
-      <label for={`question-${questionId}-4`}>
+      <label htmlFor={`question-${questionId}-4`}>
         <input type="radio" id={`question-${questionId}-4`} name={`question-${questionId}`} onChange={() => recordAnswer(questionId, 4)} />
       </label>
     </div>
